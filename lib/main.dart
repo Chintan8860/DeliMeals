aee4197f4ef2c61646d970824b574c8a3a035bd8
yash <yash1451999@gmail.com>
2021-01-22 10:18:33 +0530

    delimels

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Map<String, bool> _filters = {
-    'gluten': false,
+    'gluten': true,
     'lactose': false,
     'vegetarian': false,
     'vegan': false,
